c3cf4fe9126d8d3412cf0ed3dacb010694ad3116
chanychi <chan.ychi@gmail.com>
2023-07-12 00:43:03 -0500

    Added Boiler Plate and Tooling

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), [])
+  return <div className="app">
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      Hello
+    </ThemeProvider>
+  </div>;
+}
+
+export default App
