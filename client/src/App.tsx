778bba6017e0566f71d233b45b3bb2b75a54cc48
chanychi <chan.ychi@gmail.com>
2023-07-12 02:39:59 -0500

    Added dashboard styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,24 @@
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { useMemo } from "react";
 import { themeSettings } from "./theme";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "@/scenes/navbar";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
   return <div className="app">
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      Hello
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<div>dashboard page</div>} />
+            <Route path="/predictions" element={<div>prediction page</div>} />
+          </Routes>
+        </Box>
+      </ThemeProvider>
+    </BrowserRouter>
   </div>;
 }
 
